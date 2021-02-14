9d2c0ccbec26c75a4e011b88c680cc32c6391cca
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-02-14 10:01:19 +0100

    Created bin/yi.py

---
+++ bin/yi.py
@@ -0,0 +1,19 @@
+"""yi
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-2-14"
+
+class yi:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(yi.yi())
+	
+	def yi():
+		"""yi first funtion
+		"""
+		return "yi" #for the test (see test_yi.py)
+		
+if __name__ == "__main__":
+	yi()
